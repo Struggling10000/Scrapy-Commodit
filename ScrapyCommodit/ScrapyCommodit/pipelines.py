625d86167ff628457b5409c7f40447cc30ed00b2
tabzjh <tabzjh@gmail.com>
2018-03-25 19:43:40 +0800

    完成爬虫

---
+++ ScrapyCommodit/ScrapyCommodit/pipelines.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+from scrapy.conf import settings
+
+
+class ScrapycommoditPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+
+class MysqlPipeline(object):
+    host = 'localhost'
+    user = 'root'
+    password = ''
+    dbname = 'test'
+    db = None
+    cursor = None
+    sql = 'INSERT INTO item(itemId,itemTittle,itemPrice,itemImg) VALUES("%s","%s","%s","%s")'
+
+    def __init__(self):
+        self.host = settings['MYSQL_HOST']
+        self.user = settings['MYSQL_USER']
+        self.password = settings['MYSQL_PASSWORD']
+        self.dbname = settings['MYSQL_DB_NAME']
+
+    def open_spider(self, spider):
+        try:
+            self.db = pymysql.connect(
+                self.host, self.user, self.password, self.dbname, use_unicode=True, charset="utf8")
+            self.cursor = self.db.cursor()
+        except Exception as e:
+            spider.logger.error(e)
+
+    def close_spider(self, spider):
+        try:
+            self.db.commit()
+            self.db.close()
+        except Exception as e:
+            spider.logger.error(e)
+
+    def process_item(self, item, spider):
+        try:
+            self.cursor.execute(self.sql % (
+                item['itemId'], item['itemTittle'], item['itemPrice'], item['itemImg']))
+        except Exception as e:
+            spider.logger.error(e)
---
+++ ScrapyCommodit/ScrapyCommodit/spiders/CommoditSpider.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import logging
+import uuid
+
+import scrapy
+from scrapy.http import Request
+from scrapy.selector import Selector
+from scrapy_splash import SplashRequest
+
+from ScrapyCommodit.items import ScrapycommoditItem
+
+try:
+    from urlparse import urljoin
+except ImportError:
+    from urllib.parse import urljoin
+
+
+class CommoditspiderSpider(scrapy.Spider):
+    name = 'CommoditSpider'
+    allowed_domains = ['jd.com']
+    start_urls = [
+        'http://book.jd.com/booktop/0-0-0.html?category=1713-0-0-0-10001-1#comfort']
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse, args={'wait': 10})
+
+    def parse(self, response):
+        log = self.logger
+        try:
+            lis = response.xpath('/html/body/div[8]/div[2]/div[3]/div/ul/li')
+
+            for li in lis:
+                # print(li.extract())
+                item = ScrapycommoditItem()
+
+                item["itemId"] = uuid.uuid1()
+
+
+
+                img = li.xpath(
+                    'div[2]/a/img/@data-lazy-img').extract()[0].replace("\r\n", "").strip()
+                if img == 'done':
+                    img = li.xpath(
+                    'div[2]/a/img/@src').extract()[0].replace("\r\n", "").strip()
+                item['itemImg'] = urljoin(response.url, img)
+
+                item['itemTittle'] = li.xpath(
+                    'div[3]/a/text()').extract()[0].replace("\r\n", "").strip()
+
+                item['itemPrice'] = li.xpath(
+                    'div[3]/dl[4]/dd/em/text()').extract()[0].replace("\r\n", "").strip()
+                yield item
+        except Exception as e:
+            log.error(e)
+        try:
+            nextPage = response.xpath(
+                '/html/body/div[8]/div[2]/div[4]/div/div/span/a[7]/@href').extract()[0]
+            nextPage = urljoin(response.url, nextPage)
+            yield SplashRequest(nextPage, callback=self.parse,args={'wait': 5})
+        except Exception as e:
+            log.error(e)
